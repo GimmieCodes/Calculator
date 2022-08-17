4f3643b82a0ff8b5d7ad0e9ad39edae1a611ce6e
Dan <steezym279@gmail.com>
2022-08-17 10:37:46 -0500

    Added logic that allows calculations with decimels and improved chained calculations

--- javascript.js
+++ javascript.js
@@ -25,7 +25,8 @@ function operate (operator,a,b){
         return division(a,b)
     }
 }
-
+a = 0
+b = 0
 
 const clear = document.querySelector(".clearButton")
 clear.addEventListener("click", () =>{
@@ -35,150 +36,153 @@ clear.addEventListener("click", () =>{
 
 const one = document.querySelector("#one");
 one.addEventListener('click', () =>{
-    display.textContent += 1
-    if (i == 0){
-        display.textContent = "1"
-        return a = 0, b = 0, i = 0
-    }
+    if (a != display.textContent){
+        display.textContent += 1
+        }
+        else{
+            display.textContent = "1"
+        }
 })
 const two = document.querySelector("#two");
 two.addEventListener('click', () =>{
-    display.textContent += 2
-    if (i == 0){
-        display.textContent = "2"
-    }
+    if (a != display.textContent){
+        display.textContent += 2
+        }
+        else{
+            display.textContent = "2"
+        }
 })
 const three = document.querySelector("#three");
 three.addEventListener('click', () =>{
+    if (a != display.textContent){
     display.textContent += 3
-    if (i == 0){
+    }
+    else{
         display.textContent = "3"
     }
 })
 const four = document.querySelector("#four");
 four.addEventListener('click', () =>{
-    display.textContent += 4
-    if (i == 0){
-        display.textContent = "4"
-    }
+    if (a != display.textContent){
+        display.textContent += 4
+        }
+        else{
+            display.textContent = "4"
+        }
 })
 const five = document.querySelector("#five");
 five.addEventListener('click', () =>{
-    display.textContent += 5
-    if (i == 0){
-        display.textContent = "5"
-    }
+    if (a != display.textContent){
+        display.textContent += 5
+        }
+        else{
+            display.textContent = "5"
+        }
 })
 const six = document.querySelector("#six");
 six.addEventListener('click', () =>{
-    display.textContent += 6
-    if (i == 0){
-        display.textContent = "6"
-    }
+    if (a != display.textContent){
+        display.textContent += 6
+        }
+        else{
+            display.textContent = "6"
+        }
 })
 const seven = document.querySelector("#seven");
 seven.addEventListener('click', () =>{
-    display.textContent += 7
-    if (i == 0){
-        display.textContent = "7"
-    }
+    if (a != display.textContent){
+        display.textContent += 7
+        }
+        else{
+            display.textContent = "7"
+        }
 })
 const eight = document.querySelector("#eight");
 eight.addEventListener('click', () =>{
-    display.textContent += 8
-    if (i == 0){
-        display.textContent = "8"
-    }
+    if (a != display.textContent){
+        display.textContent += 8
+        }
+        else{
+            display.textContent = "8"
+        }
 })
 const nine = document.querySelector("#nine");
 nine.addEventListener('click', () =>{
-    display.textContent += 9
-    if (i == 0){
-        display.textContent = "9"
-    }
+    if (a != display.textContent){
+        display.textContent += 9
+        }
+        else{
+            display.textContent = "9"
+        }
 })
 const zero = document.querySelector("#zero");
 zero.addEventListener('click', () =>{
-    display.textContent += 0
-    if (i == 0){
-        display.textContent = "0"
-    }
+    if (a != display.textContent){
+        display.textContent += 0
+        }
+        else{
+            display.textContent = "0"
+        }
+})
+const decimel = document.querySelector("#decimel");
+decimel.addEventListener('click', () =>{
+    if (a != display.textContent && display.textContent.indexOf(".") != 1){
+        display.textContent += "."
+        }
+        else if (a == display.textContent){
+            display.textContent = "."
+        }
 })
 
-let i = 1
+
 
 const plus = document.querySelector("#plus");
 plus.addEventListener('click', () =>{
-    if (i < 2){
-    a = parseInt(display.textContent)
-    operator = add
+    if (a == 0){
+    a = parseFloat(display.textContent)
     console.log(a)
-    display.textContent = ""
-        i++
-        console.log(i)
-    }else if (i = 1){
+    }else{
         result()
     }
+    operator = add
 })
 
 
 
 
-// const plus = document.querySelector("#plus");
-// plus.addEventListener('click', () =>{
-//     a = parseInt(display.textContent)
-//     operator = add
-//     console.log(a)
-//     display.textContent += " + "
-//     if (i < 2){
-//         i++
-//         console.log(i)
-//     }else if (i = 1){
-//         result()
-//     }
-// })
+
 
 const minus = document.querySelector("#minus");
 minus.addEventListener('click', () =>{
-    if (i < 2){
-        a = parseInt(display.textContent)
-        operator = subtract
+    if (a == 0){
+        a = parseFloat(display.textContent)
         console.log(a)
-        display.textContent = ""
-            i++
-            console.log(i)
-        }else if (i = 1){
+        }else{
             result()
         }
+        operator = subtract
 })
 
 const times = document.querySelector("#times");
 times.addEventListener('click', () =>{
-    if (i < 2){
-        a = parseInt(display.textContent)
-        operator = multiply
+    if (a == 0){
+        a = parseFloat(display.textContent)
         console.log(a)
-        display.textContent = ""
-            i++
-            console.log(i)
-        }else if (i = 1){
+        }else{
             result()
         }
-        
+        operator = multiply
 })
 
 const divide = document.querySelector("#divide");
 divide.addEventListener('click', () =>{
-    if (i < 2){
-        a = parseInt(display.textContent)
-        operator = division
+    if (a == 0){
+        a = parseFloat(display.textContent)
         console.log(a)
-        display.textContent = ""
-            i++
-            console.log(i)
-        }else if (i = 1){
+        }else{
             result()
         }
+        operator = division
 })
 
 const equal = document.querySelector("#equal");
@@ -186,9 +190,8 @@ equal.addEventListener('click', () =>{
    result()
 })
 function result(){
-    b = parseInt(display.textContent)
-    display.textContent = operate(operator,a,b)
-    return a = 0, b = 0
+    b = parseFloat(display.textContent)
+    a = display.textContent = operate(operator,a,b)
 }
 
 
